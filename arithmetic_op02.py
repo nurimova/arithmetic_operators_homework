25c2c6635e23ca22be1b4b7cfa9b687c36d3d986
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-09-29 09:55:13 +0500

    Update arithmetic_op02.py

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,3 @@
-#Create a varaible 'number' and assign it the value of 43.
-
-#Create a variable 'answer' and assign it the sum of the number's digits.
-
-#Print the vallue of the 'answer'.
+#"n" o'zgaruvchisini yarating va unga 43 qiymatini belgilang.
+#"answer" o'zgaruvchisini yarating va unga raqam raqamlari yig'indisini belgilang.
+#"answer" qiymatini chop eting.

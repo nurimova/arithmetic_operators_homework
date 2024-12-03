3f42d0f58b6c1fc31ccf77413c99bb4e5095ee55
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-03 20:52:50 +0200

    Update arithmetic_op03.py

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,5 @@
-#"n" deb nomlangan o'zgaruvchi yarating va unga ikki xonali raqamni belgilang
+# n  deb nomlangan o'zgaruvchi yarating va unga ikki xonali raqamni belgilang
 
-#n sonining teskarisini toping va uni "answer" deb nomlangan o'zgaruvchiga belgilang.
+# n sonining teskarisini toping va uni total deb nomlangan o'zgaruvchiga belgilang.
 
-#"answer" qiymatini chop eting
+# total qiymatini chop eting

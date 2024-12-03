2f83b3fd563e622b25921699bd11346d9509398d
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-03 20:58:11 +0200

    Update arithmetic_op05.py

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,7 @@
-#Int ma'lumotlar turi bilan "raqam" deb nomlangan o'zgaruvchi yarating.
+# n  deb nomlangan o'zgaruvchi yarating va unga to'rt xonali raqamni belgilang
 
-#"Javob" deb nomlangan o'zgaruvchi yarating va unga sonning 3 ga bo'linishining qolgan qismini belgilang.
+# n sonining teskarisini toping va uni total deb nomlangan o'zgaruvchiga belgilang.
 
-#"Javob"ni chop eting.
+# sub deb nomlanga o'zgaruvchi yarating va dastlabki n qiymatdan keyingi total qiymatni ayirmasini ta'minlang.
+
+# sub o'zgaruvchisini chiqaring

44655ead4a7adef413df84b8206391f0c701e690
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-03 20:51:45 +0200

    Update arithmetic_op02.py

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,3 +1,5 @@
-#"n" o'zgaruvchisini yarating va unga 43 qiymatini belgilang.
-#"answer" o'zgaruvchisini yarating va unga raqam raqamlari yig'indisini belgilang.
-#"answer" qiymatini chop eting.
+# n o'zgaruvchisini yarating va unga 43 qiymatini belgilang.
+
+# total o'zgaruvchisini yarating va unga raqam raqamlari yig'indisini belgilang.
+
+# total qiymatini chop eting.

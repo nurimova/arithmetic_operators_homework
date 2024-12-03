7a6f140f3fb5b4d62978caf278afac769e94c855
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-03 20:54:51 +0200

    Update arithmetic_op04.py

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,11 @@
-#"Raqam" deb nomlangan o'zgaruvchi yarating va unga uch xonali raqamni belgilang.
+# n deb nomlangan o'zgaruvchi yarating va unga uch xonali raqamni belgilang.
 
-#"Raqam"ning birinchi raqamini toping va x1 ga belgilang.
+# n ning birinchi raqamini toping va x1 ga belgilang.
 
-#"Raqam"ning ikkinchi raqamini toping va x2 ga belgilang.
+# n ning ikkinchi raqamini toping va x2 ga belgilang.
 
-#"Raqam"ning uchinchi raqamini toping va x3 ga belgilang.
+# n ning uchinchi raqamini toping va x3 ga belgilang.
 
-#"Javob" deb nomlangan o'zgaruvchi yarating va unga x1, x2, x3 uchta raqam yig'indisini belgilang.
+# total deb nomlangan o'zgaruvchi yarating va unga (x1 + x2 * x3) ushbu ifodani ta'minlan.
 
-#Javobning qiymatini chop eting.
+# total qiymatini chop eting.

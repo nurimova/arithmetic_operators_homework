a774d5af5003742242c3dd633674aed00ef9cf56
nurimova <n.b.sitora@gmail.com>
2024-12-07 11:25:20 +0500

    arifmetic all

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,8 @@
 # x  o'zgaruvchisini yarating va unga 10 qiymatini belgilang.
-
+x=10
 # y  o'zgaruvchisini yarating va unga 5 qiymatini belgilang.
-
+y=5
 # total o'zgaruvchisini yarating va unga (x * y) - a + b qiymatini belgilang.
-
+total=(x*y)
 # Natija qiymatini chop eting.
+print(total)
--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,6 @@
 # n o'zgaruvchisini yarating va unga 43 qiymatini belgilang.
-
+n=43
 # total o'zgaruvchisini yarating va unga raqam raqamlari yig'indisini belgilang.
-
+total=n//10+n%10
 # total qiymatini chop eting.
+print(total)
